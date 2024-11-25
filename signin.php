276a6def6d2f3fb94f6925d68dfa39220ae795ed
Yasharth Singh <yasharthsingh0910@gmail.com>
2024-11-25 13:39:10 +0530

    2

--- signin.php
+++ signin.php
@@ -12,7 +12,7 @@ unset($_SESSION['signin-data']);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>UNDEREMPLOYED</title>
+    <title>BlogoSphere</title>
     <!-- CUSTOM STYLESHEET -->
     <link rel="stylesheet" href="./css/style.css">
     <!-- ICONSCOUT CDN -->
