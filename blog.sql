5324263c110fa84838d0a240e5ea7915a0a9dc5b
Yasharth Singh <143637324+yasharth-0910@users.noreply.github.com>
2024-11-30 00:10:12 +0530

    Update blog.sql

--- blog.sql
+++ blog.sql
@@ -1,21 +1,9 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Mar 03, 2024 at 05:41 AM
--- Server version: 10.4.27-MariaDB
--- PHP Version: 8.1.12
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `blog`
